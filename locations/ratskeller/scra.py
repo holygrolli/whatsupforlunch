b6b1f445a8b33077b035ce9dbecada1c0e631660
Andreas Groll <andreas.groll@gmail.com>
2023-07-17 17:23:56 +0200

    fix: exit if no links found

--- locations/ratskeller/scra.py
+++ locations/ratskeller/scra.py
@@ -1,3 +1,4 @@
+import sys
 import scrapy
 
 
@@ -7,7 +8,11 @@ class RatskellerSpider(scrapy.Spider):
     start_urls = ['https://ratskeller.restaurant/kantine-leipzig/speiseplan/']
 
     def parse(self, response):
-        for sel in response.xpath('//section[.//a[contains(@href,"pdf")]][1]//a[contains(@href,"pdf")]/@href'):
-            link = sel.get()
-            print(link)
-            yield {"pdf":link}
+        links = response.xpath('//section[.//a[contains(@href,"pdf")]][1]//a[contains(@href,"pdf")]/@href')
+        if (len(links) > 0):
+            for sel in links:
+                link = sel.get()
+                print(link)
+                yield {"pdf":link}
+        else:
+            sys.exit(1)
