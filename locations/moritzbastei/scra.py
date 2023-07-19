36625ba2f0b44a0ba615382f110750f19bab2ed8
Andreas Groll <andreas.groll@gmail.com>
2023-07-19 23:20:41 +0200

    feat: add moritzbastei

---
+++ locations/moritzbastei/scra.py
@@ -0,0 +1,23 @@
+import sys
+import scrapy
+import lxml.html.clean as clean
+
+safe_attrs = set(['src', 'alt', 'href', 'title', 'width', 'height'])
+kill_tags = ['object', 'iframe']
+cleaner = clean.Cleaner(safe_attrs_only=True, safe_attrs=safe_attrs, kill_tags=kill_tags)
+
+class MBSpider(scrapy.Spider):
+    name = "MB"
+    allowed_domains = ["www.moritzbastei.de"]
+    start_urls = ['https://www.moritzbastei.de/gastronomie-kneipe-bistro-drinks/']
+
+    def parse(self, response):
+        mittagsangebot = response.xpath('//div[contains(@class,"foodMenu--weekly")]')
+        if (len(mittagsangebot) == 1):
+            for sel in mittagsangebot:
+                div = sel.get()
+                cleaned_html = cleaner.clean_html(div)
+                print(cleaned_html)
+                yield {"div":cleaned_html}
+        else:
+            sys.exit(1)
