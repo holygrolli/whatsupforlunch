ea701dd31cd33717d13fd4c2a8f016e3034887e0
Andreas Groll <andreas.groll@gmail.com>
2023-10-29 22:03:18 +0100

    fix: try to mitigate public holidays

--- locations/lecasino/process_textractor_result.py
+++ locations/lecasino/process_textractor_result.py
@@ -1,4 +1,5 @@
-import os, sys
+import io, os, sys
+import pandas as pd
 from textractor.entities.document import Document
 from textractor.visualizers.entitylist import EntityList
 from textractor.data.constants import TextractFeatures, Direction, DirectionalFinderType
@@ -6,15 +7,20 @@ from textractor.data.constants import TextractFeatures, Direction, DirectionalFi
 document = Document.open(sys.argv[1])
 print(document)
 table = EntityList(document.tables[0])
-print(table[0].to_csv())
-csv = table[0].to_csv().split("\n")
-f = open("table.csv", "w")
-for i in range(1,5):
-  f.write(csv[i] + "\n")
-f.close()
-f = open("output.txt", "w")
-# first line with "Mittagsangebot"
-print(document.lines[0])
-f.write(document.lines[0].text+"\n")
+csv = table[0].to_csv()
+print(csv)
+# read csv string and transpose
+
+# Read csv string and transpose
+df = pd.read_csv(io.StringIO(csv), usecols=[1,2,3,4,5,6], skiprows=[5,6])
+print(df)
+df = df.transpose()
+
+# Write transposed data to csv file
+df.to_csv('table.csv', header=False, index=False)
+
+# Write first line with "Mittagsangebot" to output.txt
+with open('output.txt', 'w') as f:
+    f.write(document.lines[0].text + '\n')
 f.close()
 #document.tables[0].to_csv()
