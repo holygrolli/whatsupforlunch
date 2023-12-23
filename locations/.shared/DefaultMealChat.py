7c7e098c8938ed13eee42639ce5b98e4f0435cc0
Andreas Groll <andreas.groll@gmail.com>
2023-12-23 17:37:36 +0100

    feat: use gpt4-vision api

--- locations/.shared/DefaultMealChat.py
+++ locations/.shared/DefaultMealChat.py
@@ -6,8 +6,10 @@ class DefaultMealChat:
                     systemPromptFile="chatgpt_systemprompt.txt", 
                     userMessage=None, 
                     userMessageFile="chatgpt_user.txt",
-                    userMessagePrefix=""):
+                    userMessagePrefix="",
+                    max_tokens=1000):
         self.userMessagePrefix = userMessagePrefix
+        self.max_tokens = max_tokens
         if systemPrompt is None:
             file = open(systemPromptFile, "r")
             self.systemPrompt = file.read()
@@ -21,6 +23,36 @@ class DefaultMealChat:
         else:
             self.userMessage = userMessage
 
+    def writeToFile(self, chat_completion):
+        print(chat_completion.usage)
+        # print the chat completion for debugging
+        print(chat_completion.choices[0].message.content)
+        out = open ("chatgpt.json", "w")
+        out.write(chat_completion.choices[0].message.content)
+        out.close()
+
+    def processImageAndWriteToFile(self, userImage=""):
+        print("Using the prompt: " + self.systemPrompt)
+        gpt_messages=[
+            {"role": "system", "content": self.systemPrompt}]
+        gpt_messages.append({"role": "user", "content": [
+            {
+                "type": "text",
+                "text": self.userMessagePrefix + self.userMessage
+            },
+            {
+                "type": "image_url",
+                "image_url": userImage
+            }
+        ]})
+        
+        print("gpt_messages: " + str(gpt_messages))
+        chat_completion = openai.ChatCompletion.create(model="gpt-4-vision-preview",
+                                                        messages=gpt_messages,
+                                                        max_tokens=self.max_tokens)#,
+                                                        #response_format={ "type":"json_object" })
+        self.writeToFile(chat_completion)
+
     def processAndWriteToFile(self):
         # print the system prompt for debugging
         print("Using the prompt: " + self.systemPrompt)
@@ -30,10 +62,6 @@ class DefaultMealChat:
         print("gpt_messages: " + str(gpt_messages))
         chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo-1106",
                                                         messages=gpt_messages,
-                                                        response_format={ "type":"json_object" })
-        print(chat_completion.usage)
-        # print the chat completion for debugging
-        print(chat_completion.choices[0].message.content)
-        out = open ("chatgpt.json", "w")
-        out.write(chat_completion.choices[0].message.content)
-        out.close()
+                                                        response_format={ "type":"json_object" },
+                                                        max_tokens=self.max_tokens)
+        self.writeToFile(chat_completion)
--- locations/lecasino/process_chatgpt.py
+++ locations/lecasino/process_chatgpt.py
@@ -3,12 +3,19 @@ import os, sys, inspect
 sys.path.append(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + '/../.shared')
 from DefaultMealChat import DefaultMealChat
 
-ftxt = open("output.txt", "r")
-dateInput = ftxt.read()
-ftxt.close()
+import base64
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "image.png"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
 # Create an instance of DefaultMealChat
 meal_chat = DefaultMealChat(systemPromptFile=sys.argv[1], 
-                userMessageFile=sys.argv[2], 
-                userMessagePrefix="You will infer the meal offers from the following German headline text, expecting the first date being of year " + datetime.today().strftime('%Y') + ": " + dateInput + "Parse the meals of following CSV data:\n")
+                userMessage="Extract the JSON containing the meal offers from the following image!")
 
-meal_chat.processAndWriteToFile()
+meal_chat.processImageAndWriteToFile(userImage=f"data:image/png;base64,{base64_image}")
