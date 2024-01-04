6aa29e18cc799a0e398b2cb59bf841217b21f6c9
Andreas Groll <andreas.groll@gmail.com>
2024-01-04 14:01:53 +0100

    chore: externalize config
    as a base for prompt testing we need external config for re-use

--- locations/emilundmoritz/process_chatgpt.py
+++ locations/emilundmoritz/process_chatgpt.py
@@ -1,10 +1,11 @@
 import os, sys, inspect
 sys.path.append(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) + '/../.shared')
 from DefaultMealChat import DefaultMealChat
+from config import config
 
 # Create an instance of DefaultMealChat
 meal_chat = DefaultMealChat(systemPromptFile=sys.argv[1], 
                 userMessageFile=sys.argv[2], 
-                userMessagePrefix="The input is:\n")
+                **config)
 
 meal_chat.processAndWriteToFile()
