087f7039f6e6192f8b6d63521daf6db216d2f5ce
Andreas Groll <10852221+holygrolli@users.noreply.github.com>
2026-02-09 20:44:40 +0100

    fix(Augustiner): update start URL and refine PDF link extraction logic

--- locations/augustiner/scra.py
+++ locations/augustiner/scra.py
@@ -5,10 +5,10 @@ import scrapy
 class LunchSpider(scrapy.Spider):
     name = "LunchSpider"
     allowed_domains = ["www.augustiner-leipzig.de"]
-    start_urls = ['https://www.augustiner-leipzig.de/speisen/']
+    start_urls = ['https://augustiner-leipzig.de/speisekarte/']
 
     def parse(self, response):
-        links = response.xpath('//a[contains(@href,"pdf") and contains(@href,"Mittag")]/@href')
+        links = response.xpath('//a[contains(@href,"pdf") and contains(translate(. ,"ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz"), "mittagskarte")]/@href')
         if (len(links) > 0):
             for sel in links:
                 link = sel.get()
